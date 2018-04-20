af4866eb90cd961a5ce2f5e2b74dab1e8194e9a8
bobthefairycat <saiima.ali@mail.utoronto.ca>
2018-04-20 12:13:27 -0400

    Added some comments
    Updated info.txt for python excel script, and restructured make_excel.py

--- BatchWriteExcel/make_excel.py
+++ BatchWriteExcel/make_excel.py
@@ -1,36 +1,47 @@
 import os
 
 if __name__ == "__main__":
-    # where is the data?
-    data_dir = 'C:\\Users\\Saima\\Documents\\REL Projects\\Homeira Heterogeneity\\AHP\\' #need double backslash otherwise python thinks its an escape sequence
+    
+    print('Please refer to info.txt for instructions and tips')
+    
+    # ----------- MAIN DATA DIRECTORY ---------
+    data_dir = 'C:\\Users\\Saima\\Documents\\REL Projects\\Homeira Heterogeneity\\AHP\\'
+    #need DOUBLE backslash
 
-    #what folders do the data live in?
+    #----------- SUBFOLDERS WITH DATA ---------
     folder_list = ['Total_5_Homeira', 'TotaL_5_Lihua', 'Total2Homeira', 'Total2lihua']
 
-    #where to save the figures?
-    export_dir = 'ExportDir'
+    #------------ EXCEL FILE NAME ------------
+    excel_name = 'AHP_04182018.csv' #this should be a csv file extension
+    
+    #------------ HEADING SETTINGS -----------
+    blank = ' ' # can change some fields to blank if needed
 
-    #HEADINGS
+    offset = str(0)
+    anchan = str(2)
+    tags = 'None'
+    groupinc = 'yes'
+    export_dir = 'ExportDir'     #where to save the figures?
+    comment = blank
+
+    '''HEADINGS
     # Dir	                	Offset	Offset	AnChan	Comment	ExportDir	Tags	                                            GroupInc
     #Total5	20131211_600_1_0001f	0	0	2		                Cell 1, RMP - 68.5mv, L5, Tau 3, Gain 20, DC150	    yes
 
-    #this is problematic if there are nested folders or have other files...
+    #this code is problematic if there are nested folders
     #should check for nesting but I'm too lazy right now
-    
-    #some variables for writing the excel file
-    offset = str(0)
-    anchan = str(2)
-    blank = ' '
-    tags = 'None'
-    groupinc = 'yes'
+    '''
+
+    #------------ FROM HERE BELOW, DO NOT CHANGE ANYTHING!!! ------
+    headings = ['Dir', 'Offset', 'Offset', 'AnChan', 'Comment', 'ExportDir', 'Tags', 'GroupInc']
+    static_settings = [offset, offset, anchan, comment, export_dir, tags, groupinc]
 
     comma = ','
-    comment = blank
-    
-    excel_name = 'AHP.csv'
-    f = open(excel_name, 'w')
 
     print('---- Writing the excel sheet, patience is a virtue ----')
+
+    f = open(excel_name, 'w')
+    f.write(comma.join(headings) + '\n')
 
     for folder in folder_list:
         files_array = os.listdir(data_dir + folder) #have a list of all the files
@@ -39,13 +50,7 @@ if __name__ == "__main__":
                 print(file + ' is not the right file type, silly goose!! It has been skipped')
                 continue
             
-            this_line = [folder, file[0:-4] + 'f', offset, offset, anchan, comment, export_dir, tags, groupinc]
-
-            #just checking my string indexing
-            #print(file[0:-4])
-            #print(file)
-            #me = input()
-
+            this_line = [folder, file[0:-4] + 'f'] + static_settings
             f.write(comma.join(this_line) + '\n') #make it comma delimited!
 
     f.close() #close the file
